cb0eae8a19e10c6dc3ae8ac8affbcaa6e1ebb754
Justas <justas.lapiene@vinted.com>
2024-10-22 10:34:54 +0300

    enable relative line number and undo directory

--- init.lua
+++ init.lua
@@ -90,7 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.opt.clipboard = 'unnamedplus'
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
+vim.opt.undofile = true
+vim.opt.undodir = '~/.config/nvim/undo'
+vim.opt.relativenumber = true
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
